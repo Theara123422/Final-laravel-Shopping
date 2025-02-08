c09a4372e978cfa0790f8e2cef7e50e53c850369
Theara123422 <theara6574@gmail.com>
2025-02-08 13:11:14 +0700

    Finished View on Auth & Dashboard

---
+++ database/migrations/2025_02_08_050908_create_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('regular_price',8,2);
+            $table->decimal('sales_price',8,2);
+            $table->string('thumbnail');
+            $table->integer('category_id');
+            $table->string('color');
+            $table->string('size');
+            $table->text('decription');
+            $table->integer('views');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
